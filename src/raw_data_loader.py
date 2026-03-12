e4dc53d396aa513b7b0bef27b5c63908f4c9f70f
Karabo Mashile <108996938+Kb-Mash@users.noreply.github.com>
2026-03-12 17:56:48 +0000

    clean data notebook

--- src/raw_data_loader.py
+++ src/raw_data_loader.py
@@ -4,4 +4,4 @@ from pathlib import Path
 DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 
 def load_raw_data():
-    return pd.read_csv(DATA_DIR / "personal_expense_dataset.csv")
+    return pd.read_csv(DATA_DIR / "personal_expense_dataset.csv")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
